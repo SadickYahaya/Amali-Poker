16cbf1fb97d06fe956e3b2bc4d7a7a6ecafcb550
tackey2023 <152279647+tackey2023@users.noreply.github.com>
2023-12-07 12:15:45 -0800

    dashboard updated

--- amali-poker/src/App.vue
+++ amali-poker/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <NewStoryModal />
+    <PokerDashboard />
   </div>
 </template>
 
 <script setup>
 import NewStoryModal from "./components/Modals/NewStoryModal.vue";
+import PokerDashboard from "./components/PokerDashboard.vue";
 </script>
 
 <style scoped></style>
