31906dea456b9a09be836094cfe01f5301d49628
Livy-Olive <olivia.dosimey@amalitech.org>
2023-12-07 12:16:14 -0800

    UI refactored

--- amali-poker/src/components/Modals/NewStoryModal.vue
+++ amali-poker/src/components/Modals/NewStoryModal.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="modal">
     <div class="modal-content">
-      <div class="close"><span class="material-icons"> close </span></div>
       <h1>Create New Story</h1>
       <div class="textarea">
         <TextAreaComponent />
       </div>
+    </div>
+    <div class="button-container">
       <div class="button">
         <ButtonComponent
           v-for="(item, index) in buttItems"
           :key="index"
           :text="item"
+          :index="index"
         />
       </div>
     </div>
@@ -23,53 +25,43 @@ import TextAreaComponent from "../TextArea/InputFieldComponent.vue";
 // import CloseIcon from "../../assets/closeIcon.png";
 import { ref } from "vue";
 
-const buttItems = ref(["Upload", "Save"]);
+const buttItems = ref(["Save", "Cancel"]);
 </script>
 
 <style scoped>
 .modal {
   max-width: 703px;
-  flex-direction: column;
-  padding: 50px;
-  border-radius: 30px;
-  background-color: rgba(253, 245, 242, 1);
+  border-radius: 8px;
+  background-color: #ffffff;
   margin: 180px auto;
+  padding: 10px 16px;
 }
 
 h1 {
-  text-align: center;
-  color: rgba(71, 77, 102, 1);
+  color: #474d66;
   font-weight: 700px;
   font-size: 34px;
-  line-height: 41.15px;
-}
-
-.textarea {
-  margin: 30px;
+  line-height: normal;
+  font-weight: 700px;
+  margin: 24px 0 24px 0;
 }
 
 .button {
+  max-width: 86px;
   display: flex;
-  justify-content: space-around;
-  margin-top: 50px;
+  justify-content: flex-start;
+  /* margin: 30px 10px 24px 450px; */
+  gap: 16px;
 }
 
-.close {
+.button-container {
   display: flex;
   justify-content: flex-end;
-  margin-top: 5px;
-  color: gray;
+  margin: 24px;
+  margin-right: 135px;
 }
 
-span {
-  padding: 6px;
-}
-
-span:hover {
-  border-radius: 50%;
-  background-color: rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-  color: rgba(71, 77, 102, 1);
-  padding: 6px;
+.modal-content {
+  margin: 0 31px 0 31px;
 }
 </style>
