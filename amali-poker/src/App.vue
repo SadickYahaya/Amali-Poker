9a0fa87758fc812f7491ec0f00d2a1d0d0a66d3e
Livy-Olive <olivia.dosimey@amalitech.org>
2023-12-19 15:38:16 +0000

    Refactored NewSessionModal

--- amali-poker/src/App.vue
+++ amali-poker/src/App.vue
@@ -6,10 +6,4 @@
 
 <script setup></script>
 
-<style scoped>
-/* main {
-  background-image: url("assets/Group 633208.svg");
-  width: 100%;
-  height: 942px;
-} */
-</style>
+<style scoped></style>
